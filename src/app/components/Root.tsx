d1b540c5c76d80f661651b27fd60258116cd3e6b
Nony <nonybrighto@gmail.com>
2026-04-01 00:53:02 +0100

    scroll to top

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -2,10 +2,14 @@ import { Outlet } from "react-router";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { FloatingButtons } from "./FloatingButtons";
+import ScrollToTop from "./ScrollToTop";
+
+
 
 export function Root() {
   return (
     <div className="min-h-screen flex flex-col">
+       <ScrollToTop /> 
       <Header />
       <main className="flex-1">
         <Outlet />
